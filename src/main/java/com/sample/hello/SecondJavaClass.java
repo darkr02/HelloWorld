1fdfc068c6ec702a1559fe2a56da133e485755ca
Krishnendu Daripa <E004801@cigniti.com>
2023-05-08 16:06:00 -0400

    new updates

--- src/main/java/com/sample/hello/SecondJavaClass.java
+++ src/main/java/com/sample/hello/SecondJavaClass.java
@@ -3,7 +3,7 @@ package com.sample.hello;
 public class SecondJavaClass {
 
 	public static void main(String[] args) {
-		System.out.println("Second Java Class1");
+		System.out.println("Second Java Class - update");
 
 	}
 
